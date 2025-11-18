ec2e5c7da70c74dc5e8183347a52e3ac5e9147dc
carlos15b <carlossoller15@gmail.com>
2025-11-17 21:28:58 -0500

    Add coments

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,18 @@
+// Espera a que todo el contenido del DOM esté cargado antes de ejecutar el script.
 document.addEventListener('DOMContentLoaded', function() {
+    // Selecciona el botón del menú y el menú de navegación.
     const menuToggle = document.querySelector('.menu-toggle');
     const nav = document.querySelector('nav');
+
+    // Si ambos elementos existen, se añade la funcionalidad de toggle.
     if (menuToggle && nav) {
         menuToggle.addEventListener('click', function() {
+            // Alterna la clase 'active' en el menú para mostrarlo u ocultarlo.
             nav.classList.toggle('active');
+            
+            // Actualiza el atributo 'aria-expanded' para mejorar la accesibilidad.
             const isExpanded = nav.classList.contains('active');
-            menuToggle.setAttribute('aria-expanded', isExpanded)
-        })
+            menuToggle.setAttribute('aria-expanded', isExpanded);
+        });
     }
 });
